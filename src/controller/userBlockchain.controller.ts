b0e46b70b3925a22db520ac7dadbef218e958e35
izzudin26 <izzudin.ar26@gmail.com>
2022-05-24 02:18:07 +0700

    lint and add timestamp param

--- src/controller/userBlockchain.controller.ts
+++ src/controller/userBlockchain.controller.ts
@@ -1,75 +1,74 @@
-import { IUserBlockchain } from "../model/userBlockchain";
-import * as userBlockChainService from "../service/userBlockchain.service";
-import { HttpError, HttpResponse, Response } from "../types/response";
+import { IUserBlockchain } from '../model/userBlockchain'
+import * as userBlockChainService from '../service/userBlockchain.service'
+import { HttpError, HttpResponse, Response } from '../types/response'
 
 export const registerUser = async (req: any, res: any): Promise<Response> => {
-    const {userid} = req.body
-    try {
-        const user = await userBlockChainService.getUser(userid)
-        if(user){
-            return HttpResponse(200, "User Already exist")
-        }
-        await userBlockChainService.createUserBlockchain(userid)
-        return HttpResponse(200, "Success create user")
-    } catch (error) {
-    if (error instanceof Error) throw HttpError(500, error.message);
+  const { userid } = req.body
+  try {
+    const user = await userBlockChainService.getUser(userid)
+    if (user) {
+      return HttpResponse(200, 'User Already exist')
     }
+    await userBlockChainService.createUserBlockchain(userid)
+    return HttpResponse(200, 'Success create user')
+  } catch (error) {
+    if (error instanceof Error) throw HttpError(500, error.message)
+  }
 }
 
 export const getAllTransactionsFromUser = async (req: any, res: any): Promise<Response> => {
-  const { userid } = req.params;
+  const { userid } = req.params
   try {
-      const userData: IUserBlockchain = await userBlockChainService.getAllTransactions(userid)
-      if(!userData){
-          throw HttpError(404, "Not Found User")
-      }
-      const isChainValid = userBlockChainService.isValid(userData.transactionsBlock)
-      if(!isChainValid){
-          throw HttpError(500, "Broken Chain Data")
-      }
-      return HttpResponse(200, "Success Get Data", userData)
+    const userData: IUserBlockchain = await userBlockChainService.getAllTransactions(userid)
+    if (!userData) {
+      throw HttpError(404, 'Not Found User')
+    }
+    const isChainValid = userBlockChainService.isValid(userData.transactionsBlock)
+    if (!isChainValid) {
+      throw HttpError(500, 'Broken Chain Data')
+    }
+    return HttpResponse(200, 'Success Get Data', userData)
   } catch (error) {
-    if (error instanceof Error) throw HttpError(500, error.message);
+    if (error instanceof Error) throw HttpError(500, error.message)
   }
-};
+}
 
 export const insertTransactionsToUser = async (req: any, res: any): Promise<Response> => {
-    const { userid } = req.params
-    const { payload } = req.body
-    if(!userid){
-        throw HttpError(400, "missing userid params")
-    }
-    if(!payload){
-        throw HttpError(400, "missing payload body")
-    }
-    try {
-        await userBlockChainService.createTransactions(userid, payload)
-        return HttpResponse(200, "Success Insert Data")
-    } catch (error) {
-        if (error instanceof Error) throw HttpError(500, error.message);
-    }
+  const { userid } = req.params
+  const { payload, timestamp } = req.body
+  if (!userid) {
+    throw HttpError(400, 'missing userid params')
+  }
+  if (!payload) {
+    throw HttpError(400, 'missing payload body')
+  }
+  try {
+    await userBlockChainService.createTransactions(userid, payload, timestamp)
+    return HttpResponse(200, 'Success Insert Data')
+  } catch (error) {
+    if (error instanceof Error) throw HttpError(500, error.message)
+  }
 }
 
 export const getDetailTransactionUser = async (req: any, res:any): Promise<Response> => {
-    const {userid, hash} = req.params
-    try {
-        const data = await userBlockChainService.getDetailBlock(userid, hash)
-        if(!data){
-            throw HttpError(404, "Not Found User or HashBlock")
-        }
-        return HttpResponse(200, "Success get Hashblock", data)
-    } catch (error) {
-    if (error instanceof Error) throw HttpError(500, error.message);
+  const { userid, hash } = req.params
+  try {
+    const data = await userBlockChainService.getDetailBlock(userid, hash)
+    if (!data) {
+      throw HttpError(404, 'Not Found User or HashBlock')
     }
+    return HttpResponse(200, 'Success get Hashblock', data)
+  } catch (error) {
+    if (error instanceof Error) throw HttpError(500, error.message)
+  }
 }
 
 export const syncUser = async (req: any, res: any) => {
-    const {userid} = req.params
-    try {
-        await userBlockChainService.synchronize(userid)
-        return HttpResponse(200, "Processing Synchronize")
-    } catch (error) {
-    if (error instanceof Error) throw HttpError(500, error.message);
-        
-    }
+  const { userid } = req.params
+  try {
+    await userBlockChainService.synchronize(userid)
+    return HttpResponse(200, 'Processing Synchronize')
+  } catch (error) {
+    if (error instanceof Error) throw HttpError(500, error.message)
+  }
 }
--- src/service/userBlockchain.service.ts
+++ src/service/userBlockchain.service.ts
@@ -1,106 +1,77 @@
-import { UserBlockChain, IUserBlockchain } from "../model/userBlockchain";
-import * as nodeService from "./node.service";
-import { IBlockChain } from "../model/blockchain";
-import { INode } from "../model/node";
-import crypto from "crypto";
-import axios from "axios";
-import { server } from "../server";
+import { UserBlockChain, IUserBlockchain } from '../model/userBlockchain'
+import * as nodeService from './node.service'
+import { IBlockChain } from '../model/blockchain'
+import { INode } from '../model/node'
+import crypto from 'crypto'
+import axios from 'axios'
 
-export const createUserBlockchain = async (
-  userId: String
-): Promise<IUserBlockchain> => {
-  const newUser = new UserBlockChain({ userId: userId, transactionsBlock: [] });
-  return await newUser.save();
-};
+export const createUserBlockchain = async (userId: String): Promise<IUserBlockchain> => {
+  const newUser = new UserBlockChain({ userId, transactionsBlock: [] })
+  return await newUser.save()
+}
 
-export const getAllTransactions = async (
-  userId: String
-): Promise<IUserBlockchain | null> => {
-  const userdata = await UserBlockChain.findOne({ userId }).exec();
-  return userdata;
-};
+export const getAllTransactions = async (userId: String): Promise<IUserBlockchain | null> => {
+  const userdata = await UserBlockChain.findOne({ userId }).exec()
+  return userdata
+}
 
 export const getUser = async (userId: string) => {
-  return await UserBlockChain.findOne({ userId }).select("userId").exec();
-};
+  return await UserBlockChain.findOne({ userId }).select('userId').exec()
+}
 
-export const getLastBlockUser = async (
-  userId: String
-): Promise<IBlockChain | null | undefined> => {
-  const userData: IUserBlockchain | null = await UserBlockChain.findOne({
-    userId,
-  }).exec();
-  userData?.transactionsBlock.sort((a, b) => a.iteration - b.iteration);
-  return userData?.transactionsBlock.slice(-1)[0];
-};
+export const getLastBlockUser = async (userId: String): Promise<IBlockChain | null | undefined> => {
+  const userData: IUserBlockchain | null = await UserBlockChain.findOne({ userId }).exec()
+  userData?.transactionsBlock.sort((a, b) => a.iteration - b.iteration)
+  return userData?.transactionsBlock.slice(-1)[0]
+}
 
-export const getDetailBlock = async (
-  userId: Readonly<String>,
-  hashBlock: Readonly<String>
-): Promise<IBlockChain | null> => {
-  const userTransactions: IUserBlockchain = await UserBlockChain.findOne({
-    userId: userId,
-  }).exec();
-  return userTransactions.transactionsBlock.filter(
-    (b) => b.hash == hashBlock
-  )[0];
-};
+export const getDetailBlock = async (userId: Readonly<String>, hashBlock: Readonly<String>): Promise<IBlockChain | null> => {
+  const userTransactions: IUserBlockchain = await UserBlockChain.findOne({ userId }).exec()
+  return userTransactions.transactionsBlock.filter((b) => b.hash === hashBlock)[0]
+}
 
-export const createTransactions = async (userId: String, data: object) => {
-  const prevBlock: IBlockChain | null | undefined = await getLastBlockUser(
-    userId
-  );
-  const prevHash = prevBlock?.hash ?? null;
-  const iteration = (prevBlock?.iteration ?? 0) + 1;
-  const timestamp = Date.now();
+export const createTransactions = async (userId: String, data: object, timestamp: number) => {
+  const prevBlock: IBlockChain | null | undefined = await getLastBlockUser(userId)
+  const prevHash = prevBlock?.hash ?? null
+  const iteration = (prevBlock?.iteration ?? 0) + 1
   const block: IBlockChain = {
     iteration,
     timestamp,
     metadata: data,
     prevHash,
-    hash: createHash(
-      `${JSON.stringify(data)}${prevHash}${iteration}${timestamp}`
-    ),
-  };
-  await UserBlockChain.findOneAndUpdate(
-    { userId },
-    { $push: { transactionsBlock: block } }
-  );
-};
+    hash: createHash(`${JSON.stringify(data)}${prevHash}${iteration}${timestamp}`)
+  }
+  await UserBlockChain.findOneAndUpdate({ userId }, { $push: { transactionsBlock: block } }
+  )
+}
 
 const createHash = (data: String): String => {
-  const createHash = crypto.createHash("sha256");
-  return createHash.update(Uint8Array.from(Buffer.from(data))).digest("hex");
-};
+  const createHash = crypto.createHash('sha256')
+  return createHash.update(Uint8Array.from(Buffer.from(data))).digest('hex')
+}
 
 export const isValid = (transactions: IBlockChain[]): boolean => {
-  let iteration: number = 0;
+  let iteration: number = 0
   while (transactions[iteration + 1]) {
-    if (transactions[iteration].hash != transactions[iteration + 1].prevHash) {
-      return false;
+    if (transactions[iteration].hash !== transactions[iteration + 1].prevHash) {
+      return false
     }
-    iteration++;
+    iteration++
   }
-  return true;
-};
+  return true
+}
 
 export const synchronize = async (userid: Readonly<String>) => {
-  const currentData: IUserBlockchain = await getAllTransactions(userid);
-  const lengthData = currentData.transactionsBlock.length;
-  const nodes: INode[] = await nodeService.getNodes();
+  const currentData: IUserBlockchain = await getAllTransactions(userid)
+  const lengthData = currentData.transactionsBlock.length
+  const nodes: INode[] = await nodeService.getNodes()
   nodes.forEach(async (node) => {
-    try {
-      const { data, status } = await axios.get(
-        `${node.uri}/blockchain/${userid}`
-      );
-      if (data.data.transactionsBlock.length > lengthData) {
-        await UserBlockChain.updateOne(
-          { userId: userid },
-          { transactionsBlock: data.data.transactionsBlock }
-        );
-      }
-    } catch (error) {
-      throw error;
+    const { data } = await axios.get(`${node.uri}/blockchain/${userid}`)
+    if (data.data.transactionsBlock.length > lengthData) {
+      await UserBlockChain.updateOne(
+        { userId: userid },
+        { transactionsBlock: data.data.transactionsBlock }
+      )
     }
-  });
-};
+  })
+}
